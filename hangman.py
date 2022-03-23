44824e84c89621b38c01a41437fd7a5721a44aed
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-03-23 13:10:11 -0400

    big update
    added:
    saving to file
    hard mode
    normal mode
    better display
    lives
    more information collected

--- hangman.py
+++ hangman.py
@@ -2,49 +2,104 @@ import linecache
 import random
 import time
 
-# base
-word = ''
-used = []
-correct = 0
-fullWord = []
-# stats
-attempts = 12
+
+def reset():  # resets vars for new round
+    global word, used, correct, fullWord, attempts, condition
+    word = ''
+    used = []
+    correct = 0
+    fullWord = []
+    attempts = 12
+    condition = 0
 
 
-def info():
+def start():  # start function
+    global mode, autoSave
     print('---< commands >---')
     print('enter any letter as your guess')
     print('enter !guess to guess the whole word')
-    print('enter !used at any time during the game to see the letters you already used')
-    print('\n')
+    print('enter !used to see used letters')
+    print('enter !end to give and end the game')
+    print('=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+    reset()
+    time.sleep(1)
+    print('do you want to auto save results to a file?')
+    print('enter y to continue\nenter any key to skip')
+    option = input('enter your choice: ')
+    if option == 'y':
+        autoSave = True
+    else:
+        autoSave = False
+    print('!normal is for normal mode | 12 lives, full word wrong -2 lives')
+    print('!hard is for hard mode | longer words, 6 lives, full word wrong you die')
+    mode = input('enter !normal or !hard: ')
+    main()
+
+
+def again():
+    global mode, attempts, endCondition
+    option = input('enter !save to save your results\nenter !play to play again\nenter !stop to stop\n')
+    if option == '!play':
+        reset()
+        time.sleep(1)
+        print('!normal is for normal mode | 12 lives, full word wrong -2 lives')
+        print('!hard is for hard mode | longer words, 6 lives, full word wrong you die')
+        mode = input('enter !normal or !hard: ')
+        main()
+    elif option == '!save' or autoSave is True:
+        if autoSave is True:
+            print('auto saved')
+        file = open('history', 'a')
+        file.write('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=\n')
+        file.write(f'{endCondition}\n')
+        file.write(
+            f'word: {word[:-1]}\nmode: {mode[1:]}\nlives left: {lives}\nwrong letters: {attempts - lives}\nused letters: {len(used)}\n')
+        file.write(f'used letter list: {used}\n')
+        again()
+    else:
+        exit()
 
 
 def stats():  # stats for end of game
-    wrongLetter()
+    global mode, attempts, lives, endCondition
+    if attempts < 0:
+        attempts = 0
+    lives = attempts
+    if mode == '!hard':
+        attempts = 6
+    else:
+        attempts = 12
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-    print(f'word: {word[:-1]}\nlives left: {attempts}\nwrong letters: {wrongGuess}\nused letters: {len(used)}')
+    if condition == 0:  # sets end condition of the game
+        endCondition = 'you won'
+    elif condition == 1:
+        endCondition = 'you lost | you ran out of lives'
+    elif condition == 2:
+        endCondition = 'you lost | you guessed the wrong word'
+    elif condition == 3:
+        endCondition = 'you lost | you gave up'
+    else:
+        endCondition = 'you lost | no lose information'
+    print(endCondition)
+    print(
+        f'word: {word[:-1]}\nmode: {mode[1:]}\nlives left: {lives}\nwrong letters: {attempts - lives}\nused letters: {len(used)}')
+    print(f'used letter list: {used}')
     time.sleep(1)
     print('\ncheck out my github: https://github.com/cqb13')
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-    time.sleep(20)
-    exit()
+    again()
 
 
-def wrongLetter():  # finds the amount of wrong letter I think
-    global wrongGuess
-    num = 0
-    wrongGuess = (len(used))
-    for i in used:
-        if used[num] in word:
-            wrongGuess -= 1
-        else:
-            pass
-        num += 1
-
-
-def options():
-    global fullWord, word, correct, attempts
+def main():
+    global fullWord, word, correct, attempts, condition
     word = linecache.getline('words', random.randrange(0, 8749))
+    if mode == '!hard':
+        print('you are on hard mode')
+        attempts = 6
+        while len(word) <= 6:
+            word = linecache.getline('words', random.randrange(0, 8749))
+    else:
+        print('you are on normal mode')
     wordlength = len(word)
     while wordlength != 0:  # creates the word visual
         fullWord.append('_')
@@ -55,43 +110,59 @@ def options():
     print(f'lives: {attempts}')
 
     while attempts > 0:
-        option = input('enter a guess')
+        option = input('enter a guess: ')
 
         if option == '!used':
             print(f'used letters: {used}')
+        elif option == '!end':
+            print('are you sure you want to give up?')
+            print('enter y to give up\nenter any key to skip')
+            option = input('enter your choice: ')
+            if option == 'y':
+                condition = 3
+                stats()
+            else:
+                print('skipped')
         elif option == '!guess':  # option to guess the full word
-            print('if you get the word wrong you will lose.\npress y to continue '
-                  'or x to skip')
-            option = input('y to continue, or any key to skip')
+            if mode == '!hard':
+                print('if incorrect you will lose')
+            else:
+                print('if incorrect you will lose 2 lives')
+            print('enter y to give up\n enter any key to skip')
+            option = input('enter your choice: ')
             word = word[:-1]  # something wrong with word, idk anymore
             if option == 'y':  # makes sure that you actually want to do it
-                option = input('enter word')
+                option = input('enter word: ')
                 if option == word:  # checks if the word is right
-                    print('you won')
+                    condition = 0
                     stats()
+                elif option != word and mode != '!hard':  # checks if you are on normal mode
+                    print('that\'s not the right word | -2 attempts')
+                    attempts -= 2
+                    print(f'lives: {attempts}')
+                    if attempts < 0:
+                        condition = 1
+                        stats()
                 else:
-                    print('you lost')
+                    condition = 2
                     stats()
             else:
                 print('skipped')
         else:
             if len(option) > 1:  # checks that you only entered 1 letter
-                print('enter !full to guess the word')
+                print('enter !guess to guess the full word')
             else:
                 letterUsed = False  # I don't fucking know, it does something I think
                 num = 0
-                for i in used:  # checks if letter has been used before
+                for _ in used:  # checks if letter has been used before
                     if option == used[num]:
                         print(f'letter {option} has already been used')
                         print('enter !used to see all used letters')
                         letterUsed = True
-                        pass
-                    else:
-                        pass
                     num += 1
 
                 num = 0
-                for i in word:  # adds letter to its spot in hidden word
+                for _ in word:  # adds letter to its spot in hidden word
                     if letterUsed is True:
                         pass
                     elif option == word[num]:  # weird word symbol thing
@@ -99,14 +170,10 @@ def options():
                         temp[num] = option
                         fullWord = ''.join(temp)
                         correct += 1
-                    else:
-                        pass
                     num += 1
 
                 if option not in word and option not in used:  # checks if it should take away a life
                     attempts -= 1
-                else:
-                    pass
 
                 print(fullWord)
 
@@ -114,23 +181,18 @@ def options():
                     pass
                 else:  # adds every letter to list
                     used.append(option)
-                if correct == len(word) - 1:  # checks if you won
-                    print('you won')
+                if fullWord == word[:-1]:  # checks if you won
+                    condition = 0
                     stats()
                 elif attempts <= 0:  # checks if you have too many wrong letters
-                    print('you lost')
+                    condition = 1
                     stats()
-                else:
-                    pass
 
                 print(f'lives: {attempts}')
                 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 
 
 print('---< welcome to hangman >---')
-print('---<   made by: cqb13   >---')
-print('\n')
+print('---<   made by: cqb13   >---\n')
 time.sleep(1)
-info()
-time.sleep(1)
-options()
+start()
